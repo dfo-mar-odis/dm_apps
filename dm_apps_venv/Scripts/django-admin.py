aba6f8f2e7ea1bf1515a108798c3e41fb6937c2e
Thompson <THOMPSONE@dfo-mpo.ca>
2020-02-11 16:05:23 -0400

    remove venv from project folder (oooops)

--- dm_apps_venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\thompsone\documents\github\dm_apps\dm_apps_venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
