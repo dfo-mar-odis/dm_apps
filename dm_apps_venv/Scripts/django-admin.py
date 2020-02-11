75d42e8f2cbc1682d5df060ab87556c3ce97bc1c
Thompson <THOMPSONE@dfo-mpo.ca>
2020-02-11 10:23:38 -0400

    copying files

---
+++ dm_apps_venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\thompsone\documents\github\dm_apps\dm_apps_venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
